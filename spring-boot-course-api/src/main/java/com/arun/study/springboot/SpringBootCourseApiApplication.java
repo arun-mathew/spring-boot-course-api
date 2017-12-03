3e4b833f2d61c94e88d4011bab17899777ee1379
arun.mathew <arunkmathewulikkal@gmail.com>
2017-12-03 09:12:12 +0530

    initial commit of spring-boot-course-api

---
+++ spring-boot-course-api/src/main/java/com/arun/study/springboot/SpringBootCourseApiApplication.java
@@ -0,0 +1,12 @@
+package com.arun.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCourseApiApplication.class, args);
+	}
+}
